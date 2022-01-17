bc44405fa58e439faf3f9722ccb68199dbd10c47
MAYUKA-UENO <wishwishjanuary@yahoo.co.jp>
2022-01-17 22:48:54 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    //キューブの移動速度
+    private float speed = -12;
+
+    //消滅位置
+    private float deadLine = -10;
+
+    //地面のいち
+    private float groundLevel = -3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        //キューブの設置状態確認
+        bool isGround = (transform.position.y == this.groundLevel) ? false : true;
+
+
+        //キューブ接地時に効果音を鳴らす
+        GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+
+
+        //画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+
+            Destroy(gameObject);
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        //　キューブと地面に衝突した場合
+        if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "GroundTag")
+        {
+            //効果音を再生
+            GetComponent<AudioSource>().Play();
+
+        }
+
+
+    }
+}
